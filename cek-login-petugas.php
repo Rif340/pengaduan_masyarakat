e44107be3ee39388295d37d2b0f3f5fdd1d24d85
To Be Human <67031062+Rif340@users.noreply.github.com>
2023-08-30 11:43:58 +0700

    Update cek-login-petugas.php

--- cek-login-petugas.php
+++ cek-login-petugas.php
@@ -2,7 +2,7 @@
 session_start();
 
 
-if(!isset($_SESSION['username']) AND !isset($_SESSION['id_petugas'])){
+if(!isset($_SESSION['username']) AND !isset($_SESSION['level'])){
     // echo "tidak ada ssesion <br />";
     header("location:login.php?pesan=Silahkan Login Terlebih Dahulu");
 }
